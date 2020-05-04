77fd42b7fd6ab0dfbe64dc90e4402932e175ff5f
Davide-bll <64286540+Davide-bll@users.noreply.github.com>
2020-05-04 23:43:55 +0200

    Output activity ratio + inputactivityratio
    Fixed O/I activity ratio. The column of the fuel is now correct.

--- Rscripts/structure3/str3.R
+++ Rscripts/structure3/str3.R
@@ -27,38 +27,56 @@ s3list <- map(s3names, ~read_osemosys(paste(path, .x, sep = "/"))) %>%
 s3list <- map(s3list, convert_struct3)
 s3list <- map(s3list, ~mutate_at(., vars('2060'), as.numeric))
 
+# disaggregate the 9 letters tag.. treat diffeent outputactivityratio
+prds <- names(s3list)[c(1,2,4)]
 # convert to std mode_of_operation
-s3list <- map(s3list , ~convert_tb(., col = "technology", 
-                          into = c("country", "fuel", "technology", "altro"), 
-                          sep = c(2,4,6)))
+s3list[prds] <- map(s3list[prds] , ~convert_tb(., col = "technology", 
+                                               into = c("country", "fuel", "technology", "altro"), 
+                                               sep = c(2,4,6)))
 # aggregate----
 actions <- list(mean,
                 mean,
-                mean,
-                sum
-                )
+                sum)
 
 grps <- list(
-  c("country", "technology", "fuel", "mode_of_operation", "year"),
   c("country", "technology", "fuel", "mode_of_operation", "year"),
   c("country", "technology", "fuel", "mode_of_operation", "year"),
   c("country", "technology", "fuel", "mode_of_operation", "year")
 )
 
 # arguments
-args <- list(x   = s3list, 
+args <- list(x   = s3list[prds], 
              grp = grps, 
              f   = actions)
 
 # s3list
-s3list <- pmap(args, my_group_by)
+s3list[prds] <- pmap(args, my_group_by)
 
 # add emission in emissionactivityratio
 s3list$EmissionActivityRatio <- s3list$EmissionActivityRatio %>% 
   mutate(emission = "CO2") %>% 
   select(country, emission, technology, fuel, mode_of_operation, year, value)
 
+# outputactivityratio-----
+# this is a structure 4 btw
+s3list$OutputActivityRatio <- s3list$OutputActivityRatio %>% 
+  convert_tb(col = "technology", into = c("country", "technology", "altro", "fuel"), sep = c(2,6,12)) %>%
+  filter(!endsWith(technology, "00")) %>% 
+  group_by(country, technology, fuel, mode_of_operation, year) %>% 
+  summarise(value = mean(value, na.rm = TRUE)) %>% 
+  ungroup()
+
+# ABCDE -> DEABC 
+revert_string <- function(x, l = 2) {
+  purrr::map_chr(x, ~c(substring(., l + 1, nchar(.)), substring(., 1, l)) %>% 
+                   paste(collapse = ""))
+}
+
+
+s3list$OutputActivityRatio <- s3list$OutputActivityRatio %>% 
+  mutate(technology = revert_string(technology))
+
 # # write csv
- # imap(s3list, ~write.csv(.x, 
- #                         paste("C:/Users/Utente/Desktop/gamsathome/data/params_gams/", paste0(.y, ".csv"),  sep = "/"), 
- #               row.names = FALSE))
+# imap(s3list, ~write.csv(.x,
+#                         paste("C:/Users/Utente/Desktop/gamsathome/data/params_gams/", paste0(.y, ".csv"),  sep = "/"),
+#               row.names = FALSE))
